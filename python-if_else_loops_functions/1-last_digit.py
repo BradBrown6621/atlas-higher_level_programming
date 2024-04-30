1d336d4e41c78e6ddee147ee4f3f177947f9cf0d
BradBrown6621 <6621@holbertonstudents.com>
2024-04-30 14:23:59 -0500

    1-: No longer alters the original integer input (i think)

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+last = abs(number) % 10
 if (last > 5):
     numProperty = "greater than 5"
 elif ((last < 6) and (last != 0)):
