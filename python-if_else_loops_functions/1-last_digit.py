46bbf7341748cdb403b55ae3a9e2c2cac9eb35ee
BradBrown6621 <6621@holbertonstudents.com>
2024-04-30 14:27:51 -0500

    1-: Negative numbers are less than 0, buddy, so the output should show that

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
+if (number < 0):
+    last *= -1
 if (last > 5):
     numProperty = "greater than 5"
 elif ((last < 6) and (last != 0)):
     numProperty = "less than 6 and not 0"
 elif (last == 0):
     numProperty = "0"
-if (number < 0):
-    last *= -1
 print(f"Last digit of {number} is {last} and is {numProperty}")
