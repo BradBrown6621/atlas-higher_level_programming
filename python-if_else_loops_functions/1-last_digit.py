3773171b47c6f501ce601bec4883214ad97b1ee6
BradBrown6621 <6621@holbertonstudents.com>
2024-04-30 14:11:59 -0500

    1-: Converts root     pts/1                         Sun Apr 28 21:27    gone - no logout reboot   system boot  5.15.146.1-micro Sun Apr 28 21:27   still running root     pts/1                         Fri Apr 12 10:11 - crash (16+11:15) reboot   system boot  5.15.146.1-micro Fri Apr 12 10:11   still running root     pts/1                         Fri Mar 22 10:00 - crash (21+00:11) reboot   system boot  5.15.146.1-micro Fri Mar 22 09:59   still running root     pts/1                         Sat Mar 16 09:58 - crash (6+00:01) reboot   system boot  5.15.146.1-micro Sat Mar 16 09:58   still running root     pts/1                         Fri Mar 15 09:06 - crash (1+00:52) reboot   system boot  5.15.146.1-micro Fri Mar 15 09:06   still running root     pts/1                         Mon Mar 11 12:17 - crash (3+20:48) reboot   system boot  5.15.146.1-micro Mon Mar 11 12:17   still running root     pts/1                         Fri Mar  8 09:48 - crash (3+01:29) reboot   system boot  5.15.146.1-micro Fri Mar  8 09:48   still running root     pts/1                         Tue Feb 13 21:07 - crash (23+12:40) reboot   system boot  5.15.133.1-micro Tue Feb 13 21:07   still running root     pts/1                         Wed Dec 20 10:01 - crash (55+11:06) reboot   system boot  5.15.133.1-micro Wed Dec 20 10:01   still running root     pts/1                         Wed Dec 13 13:12 - crash (6+20:48) reboot   system boot  5.15.133.1-micro Wed Dec 13 13:12   still running root     pts/1                         Wed Nov 29 15:55 - crash (13+21:17) reboot   system boot  5.15.133.1-micro Wed Nov 29 15:55   still running root     pts/1                         Mon Nov 20 11:28 - crash (9+04:27) reboot   system boot  5.15.133.1-micro Mon Nov 20 11:28   still running root     pts/1                         Mon Nov 20 11:14 - crash  (00:13) reboot   system boot  5.15.133.1-micro Mon Nov 20 11:14   still running root     pts/1                         Tue Nov  7 13:25 - crash (12+21:48) reboot   system boot  5.15.90.1-micros Tue Nov  7 13:25   still running root     pts/1                         Tue Nov  7 13:24 - crash  (00:00) reboot   system boot  5.15.90.1-micros Tue Nov  7 13:24   still running root     pts/1                         Tue Nov  7 13:14 - crash  (00:09) reboot   system boot  5.15.90.1-micros Tue Nov  7 13:14   still running
    wtmp begins Tue Nov  7 13:14:38 2023 to negative integer if  is negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = int((str(number)[-1].strip('-')))
+last = number % 10
 if (last > 5):
     numProperty = "greater than 5"
 elif ((last < 6) and (last != 0)):
     numProperty = "less than 6 and not 0"
 elif (last == 0):
     numProperty = "0"
+if (number < 0):
+    last *= -1
 print(f"Last digit of {number} is {last} and is {numProperty}")
