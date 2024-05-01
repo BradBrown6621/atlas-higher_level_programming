a48867fd4c30787d7ffd2339dc5171b162b84f7b
BradBrown6621 <6621@holbertonstudents.com>
2024-05-01 17:23:11 -0500

    9-print_last_digit.py: Instead of doing nothing on incorrect input types, now it just breaks

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if type(number) is int:
-        print("{}".format(abs(number) % 10), end="")
-        return (abs(number) % 10)
+    print("{}".format(abs(number) % 10), end="")
+    return (abs(number) % 10)
