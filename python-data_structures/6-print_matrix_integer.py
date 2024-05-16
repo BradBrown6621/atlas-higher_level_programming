87d8b9574ccd4566efea571db15325d2fb2188a6
BradBrown6621 <6621@holbertonstudents.com>
2024-05-16 09:49:03 -0500

    6-print_matrix_integer.py: Created first draft

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(0, len(matrix)):
+        for j in matrix[i]:
+            print(j, end="")
+            if j is not matrix[i][-1]:
+                print(" ", end="")
+        print("")
