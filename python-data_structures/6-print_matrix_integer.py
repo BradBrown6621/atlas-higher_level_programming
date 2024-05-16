c498165d40da35f45c375a32acfe6c30f1a837f0
BradBrown6621 <6621@holbertonstudents.com>
2024-05-16 09:53:20 -0500

    6-print_matrix_integer.py: Ensured print statements only printed integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix)):
         for j in matrix[i]:
-            print("{}".format(j), end="")
+            print("{:d}".format(j), end="")
             if j is not matrix[i][-1]:
                 print(" ", end="")
         print("")
