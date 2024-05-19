65dbebca1a32d8ba59c6a9f1be8e81e1384c095f
BradBrown6621 <6621@holbertonstudents.com>
2024-05-19 11:42:48 -0500

    1-square.py: Created first draft

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains a single Square class
+"""
+
+
+class Square:
+    """
+    Square: A class defining a Square
+    """
+    def __init__(self, size):
+        """
+        Creates attribute(s) on instantiation
+        """
+        self.__size = size
