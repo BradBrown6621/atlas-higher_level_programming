a63c87f7f8e56dfd85168d26372db5800392d272
BradBrown6621 <6621@holbertonstudents.com>
2024-05-21 09:54:19 -0500

    3-is_kind_of_class.py: Created first draft

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains 1 method:
+    -   ('is_kind_of_class')
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    A method that returns true if 'obj' is an instance of 'a_class' or any of it's superclasses.
+    """
+    return isinstance(obj, a_class)
