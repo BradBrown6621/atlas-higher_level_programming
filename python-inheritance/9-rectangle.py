38d8b961055a9da633d470a33c57d049b2c3fa0e
BradBrown6621 <6621@holbertonstudents.com>
2024-05-21 16:19:35 -0500

    9-rectangle.py: Created first draft

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module defines 1 class:
+    - 'Rectangle(BaseGeometry)'
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    This class contains 4 custom methods:
+        - '__init__(self, width, height)'
+        - 'area(self)'
+        - 'print(self)'
+        - 'str()'
+    """
+
+    def __init__(self, width, height):
+        try:
+            self.integer_validator("width", width)
+        except Exception as e:
+            raise e
+        else:
+            self.__width = width
+
+        try:
+            self.integer_validator("height", height)
+        except Exception as e:
+            raise e
+        else:
+            self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __print__(self):
+        print(str(self))
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
