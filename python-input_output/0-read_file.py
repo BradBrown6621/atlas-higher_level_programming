ae5e0964c9a7fed1f49de81dce1afc8aed511232
BradBrown6621 <6621@holbertonstudents.com>
2024-05-21 17:11:41 -0500

    0-read_file.py: Documented read_file()

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,8 @@ This module contains 1 custom method
 
 
 def read_file(filename=""):
+    """
+    This method reads a filename without exception handling
+    """
     with open(filename, encoding="utf-8") as my_file:
         print(my_file.read())
