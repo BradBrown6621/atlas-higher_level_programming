c29a0d63b1becbce83ad09989ee77b2f5638b0de
BradBrown6621 <6621@holbertonstudents.com>
2024-05-21 10:47:59 -0500

    6-base_geometry.py: Created first class BaseGeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains 0 methods and 1 class 'BaseGeometry'
+"""
+
+
+class BaseGeometry():
+    """
+    This class contains 1 custom method:
+        - 'area()'
+    """
+
+    def area():
+        raise Exception("area() is not implemented")
