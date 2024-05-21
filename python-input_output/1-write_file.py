e5a4a15e8e9789ea5ba9f169f0e19365a9ef9294
BradBrown6621 <6621@holbertonstudents.com>
2024-05-21 17:20:57 -0500

    1-write_file.py: Enforced PEP8 styling on function documentation

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,7 +7,8 @@ This module contains 1 custom method:
 
 def write_file(filename="", text=""):
     """
-    This method writes a string 'text' to a text file 'filename' in UTF8 formatting
+    This method writes a string 'text'
+    to a text file 'filename' in UTF8 formatting.
     """
 
     with open(filename, mode="w", encoding="utf-8") as my_file:
