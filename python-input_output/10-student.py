419f4830015b0a7ea54de135699fc904e873c663
BradBrown6621 <6621@holbertonstudents.com>
2024-05-21 20:06:56 -0500

    10-student.py: Created first draft

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines 1 custom class:
+    - 'Student()'
+"""
+
+
+class Student():
+    """
+    This class defines a Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Called when we initialize new Student instances.
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This method gives us a dictionary representation a
+        Student's attributes.
+        """
+        if attrs is None:
+            return self.__dict__
+        return {key: value
+                for key, value in self.__dict__.items()
+                if key in attrs}
