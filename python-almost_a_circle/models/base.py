9852de6dd2d39be15f9596d94ed6eab332123298
BradBrown6621 <6621@holbertonstudents.com>
2024-05-27 15:25:31 -0500

    Created 'models' folder and added 'base.py'

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!usr/bin/python3
+"""
+This module contains 1 custom class:
+    - 'Base'
+"""
+
+
+class Base():
+    """
+    This class is a Base class for our geometries
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        This function defines a class constructor
+        """
+
+        if (id is None):
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
+        else:
+            self.id = id
