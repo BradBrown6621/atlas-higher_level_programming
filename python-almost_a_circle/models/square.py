87e9b2d9e115225aba7df966baa39a650c4b65f0
BradBrown6621 <6621@holbertonstudents.com>
2024-05-28 22:22:27 -0500

    models/square.py: class 'Square': Overwrote method '__str__' and gave it custom getters/setters for 'size'

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -20,3 +20,32 @@ class Square(Rectangle):
         """
 
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """
+        Getter for Square.__width
+        """
+
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """
+        Setter for Square.__width
+        """
+
+        self.width = size
+        self.height = size
+
+    def __str__(self):
+        """
+        String representation of a 'Square' instance
+        """
+
+        return "[Square] ({}) {}/{} - {}".format(
+                                                self.id,
+                                                self.x,
+                                                self.y,
+                                                self.size
+                                                )
