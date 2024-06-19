83915fbb3c704bfe72296eb550e6de2a1f50813d
BradBrown6621 <6621@holbertonstudents.com>
2024-06-19 18:10:49 -0500

    13-count_shows_by_genre.sql: Since we only want to include genres who have at least 1 show in their catalog, switched to RIGHT JOIN to join onto 'tv_show_genres'

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- 13. Number of shows by genre
 -- Brad Brown
-SELECT tv_genres.name, COUNT(tv_shows.title) AS number_of_shows
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
