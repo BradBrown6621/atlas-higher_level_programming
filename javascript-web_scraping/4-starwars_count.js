47b484fe894b85a5339e007b21d188b02b69bf01
BradBrown6621 <6621@holbertonstudents.com>
2024-07-27 14:07:09 -0500

    4-starwars_count.js: Removed unnecessary API requests

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,6 @@ const request = require('request');
 const requestURL = argv[2];
 
 let countWA = 0;
-let completedRequests = 0;
 
 request(requestURL, function (error, response, body) {
   if (error || response.statusCode !== 200) {
@@ -21,21 +20,12 @@ request(requestURL, function (error, response, body) {
     }
     const count = bodyData.count;
 
-    for (let x = 1; x <= count; x++) {
-      request(`${requestURL}/${x}`, function (error, response, body) {
-        if (error || response.statusCode !== 200) {
-          console.error('error: ', error);
-        } else {
-          const bodyCharacters = (JSON.parse(body).characters);
-          if (bodyCharacters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-            countWA++;
-          }
-        }
-        completedRequests++;
-        if (completedRequests === count) {
-          console.log(countWA);
-        }
-      });
+    for (let x = 0; x < count; x++) {
+      const result = bodyData.results[x];
+      if (result.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        countWA++;
+      }
     }
+    console.log(countWA);
   }
 });
