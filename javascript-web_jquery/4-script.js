e9a2f19e5f1a8c46f4cc71dcbc31beaebb4b445c
BradBrown6621 <6621@holbertonstudents.com>
2024-07-31 00:01:48 -0500

    4-script.js: Makes a button toggle a color

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+$(function () {
+  $('div#toggle_header').on('click', function () {
+    if ($('header').hasClass('red')) {
+      $('header').removeClass('red').addClass('green');
+    } else {
+      $('header').removeClass('green').addClass('red');
+    }
+  });
+});
